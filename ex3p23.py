dd4a9124cea05d36d12bf517770fb34ff280b20a
Tekeh <te276@cam.ac.uk>
2020-10-18 09:51:41 +0100

    Partial push for remote access. To be squashed later

--- ex3p23.py
+++ ex3p23.py
@@ -3,7 +3,14 @@ import codecs
 import random
 import time
 
-def untemper(z):
-    """Inverse of the temper function in the Mersenne Twister algorithm"""
+def untemper(z, mt):
+    """Inverse of the temper function in the Mersenne Twister algorithm. Takes an MT PRNG object"""
+    z = z ^ (  z >> mt.l )
+    z = z ^ ( (z << mt.t) & mt.c)
+    z = z ^ ( (z << mt.s) & mt.b)
+    y = z ^ ( (z >> mt.u) & mt.d)
+    return y
 
+
+    
     
